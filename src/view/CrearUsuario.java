5df1e550fca71227a0a89809e71c0fc8f56c5675
Manuel Ponsa <Manuel Ponsa@192.168.1.14>
2018-06-02 22:53:42 -0300

    Interfaces

--- src/view/CrearUsuario.java
+++ src/view/CrearUsuario.java
@@ -3,10 +3,19 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class CrearUsuario {
 
-	private JFrame frame;
+	private JFrame frmListaDeRegalos;
+	private JTextField textField;
+	private JLabel lblEmail;
+	private JTextField textField_1;
+	private JLabel lblFechaDeNacimiento;
+	private JTextField textField_2;
+	private JLabel lblPassword;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class CrearUsuario {
 			public void run() {
 				try {
 					CrearUsuario window = new CrearUsuario();
-					window.frame.setVisible(true);
+					window.frmListaDeRegalos.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,51 @@ public class CrearUsuario {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmListaDeRegalos = new JFrame();
+		frmListaDeRegalos.setTitle("Lista de Regalos");
+		frmListaDeRegalos.setBounds(100, 100, 450, 300);
+		frmListaDeRegalos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmListaDeRegalos.getContentPane().setLayout(null);
+		
+		JLabel lblNuevoUsuario = new JLabel("Nuevo usuario");
+		lblNuevoUsuario.setBounds(10, 11, 69, 14);
+		frmListaDeRegalos.getContentPane().add(lblNuevoUsuario);
+		
+		textField = new JTextField();
+		textField.setBounds(124, 36, 86, 20);
+		frmListaDeRegalos.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(20, 39, 46, 14);
+		frmListaDeRegalos.getContentPane().add(lblNewLabel);
+		
+		lblEmail = new JLabel("E-Mail");
+		lblEmail.setBounds(20, 70, 46, 14);
+		frmListaDeRegalos.getContentPane().add(lblEmail);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(124, 67, 86, 20);
+		frmListaDeRegalos.getContentPane().add(textField_1);
+		
+		lblFechaDeNacimiento = new JLabel("Fecha de nacimiento");
+		lblFechaDeNacimiento.setBounds(20, 107, 98, 14);
+		frmListaDeRegalos.getContentPane().add(lblFechaDeNacimiento);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(124, 104, 86, 20);
+		frmListaDeRegalos.getContentPane().add(textField_2);
+		
+		lblPassword = new JLabel("Password");
+		lblPassword.setBounds(20, 145, 46, 14);
+		frmListaDeRegalos.getContentPane().add(lblPassword);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(124, 142, 86, 20);
+		frmListaDeRegalos.getContentPane().add(textField_3);
 	}
 
 }
--- src/view/Login.java
+++ src/view/Login.java
@@ -3,10 +3,17 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login {
 
-	private JFrame frame;
+	private JFrame frmListaDeRegalos;
+	private JTextField emailField;
+	private JTextField passwordField;
 
 	/**
 	 * Launch the application.
@@ -16,7 +23,7 @@ public class Login {
 			public void run() {
 				try {
 					Login window = new Login();
-					window.frame.setVisible(true);
+					window.frmListaDeRegalos.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +42,47 @@ public class Login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmListaDeRegalos = new JFrame();
+		frmListaDeRegalos.setTitle("Lista de Regalos");
+		frmListaDeRegalos.setBounds(100, 100, 450, 300);
+		frmListaDeRegalos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmListaDeRegalos.getContentPane().setLayout(null);
+		
+		JLabel lblEmail = new JLabel("email");
+		lblEmail.setBounds(10, 36, 46, 14);
+		frmListaDeRegalos.getContentPane().add(lblEmail);
+		
+		JLabel lblPassword = new JLabel("password");
+		lblPassword.setBounds(10, 61, 46, 14);
+		frmListaDeRegalos.getContentPane().add(lblPassword);
+		
+		emailField = new JTextField();
+		emailField.setBounds(66, 33, 86, 20);
+		frmListaDeRegalos.getContentPane().add(emailField);
+		emailField.setColumns(10);
+		
+		passwordField = new JTextField();
+		passwordField.setColumns(10);
+		passwordField.setBounds(66, 58, 86, 20);
+		frmListaDeRegalos.getContentPane().add(passwordField);
+		
+		JLabel lblIniciarSesin = new JLabel("Iniciar sesi\u00F3n");
+		lblIniciarSesin.setBounds(10, 11, 142, 14);
+		frmListaDeRegalos.getContentPane().add(lblIniciarSesin);
+		
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String mail = emailField.getText();
+				String password = passwordField.getText();
+					
+			}
+		});
+		btnIngresar.setBounds(10, 86, 89, 23);
+		frmListaDeRegalos.getContentPane().add(btnIngresar);
+		
+		JButton btnCrearUsuario = new JButton("Crear usuario..");
+		btnCrearUsuario.setBounds(10, 227, 142, 23);
+		frmListaDeRegalos.getContentPane().add(btnCrearUsuario);
 	}
-
 }
