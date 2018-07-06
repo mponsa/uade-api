7fffd833aec7b931bff58aef806a46f1318a0289
MANU PONSA <manuponsa@gmail.com>
2018-07-06 20:28:18 -0300

    MP - Ultimos cambios

--- src/facade/Facade_Mobile.java
+++ src/facade/Facade_Mobile.java
@@ -1,9 +1,17 @@
 package facade;
 
+import java.util.Date;
+
 import controlador.*;
 
 public class Facade_Mobile {
 	
+	public void crearUsuario(String Nombre, String Apellido, Date FechaNac, String Mail, String password){
+		ControladorDeUsuarios.getInstancia().crearUsuario(Nombre,Apellido, FechaNac, Mail, password,true);
+	}
 	
+	public void crearLista(String nombre, Date vigencia, String agasajado, float monto, boolean estado, boolean activo, float montoPorParticipante){
+		ControladorDeLista.getInstancia().crearLista(nombre, vigencia, agasajado, monto, estado, activo, montoPorParticipante);
+	}
 	
 }
